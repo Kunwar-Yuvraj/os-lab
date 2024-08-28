2c033a0dc2ad1e038d5b18d81317b6014e6bbf7f
Kunwar Yuvraj <63793259+Kunwar-Yuvraj@users.noreply.github.com>
2024-08-28 09:42:52 +0530

    Update parent9b.c

--- parent9b.c
+++ parent9b.c
@@ -1,39 +1,33 @@
 // parent.c
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-#define SHM_KEY 1234
-#define MAX 100
+#include <sys/wait.h>
 
 int main() {
-    int shmid, *shm_ptr, num;
-    pid_t pid;
+    int N = 10; 
+    key_t key = 1234;
+    int shmid;
+    int *shm_addr;
 
-    // Create or get shared memory
-    shmid = shmget(SHM_KEY, MAX * sizeof(int), 0666);
+    shmid = shmget(key, N * sizeof(int), IPC_CREAT | 0666);
 
-    // Fork and execute child
-    if ((pid = fork()) == 0) {
-        execl("./child", "child", NULL);
-        perror("execl");
-        exit(1);
+    if (fork() == 0) {
+        execl("./oddgen", "oddgen", NULL);
     }
+
     wait(NULL);
 
-    // Attach shared memory
-    shm_ptr = (int *)shmat(shmid, NULL, 0);
-
-    // Read number of odd numbers
-    num = shm_ptr[0];
-    printf("Odd numbers:\n");
-    for (int i = 1; i <= num; i++) printf("%d ", shm_ptr[i]);
+    shm_addr = (int *)shmat(shmid, NULL, 0);
+    for (int i = 0; i < N; i++) {
+        printf("%d ", shm_addr[i]);
+    }
     printf("\n");
 
-    // Cleanup
-    shmdt(shm_ptr);
+    shmdt(shm_addr);
     shmctl(shmid, IPC_RMID, NULL);
+
     return 0;
 }
